bea2105f41cbe66514864936b955920c72d80fc1
Ali-consciousroad <ali_dindar@live.be>
2022-01-17 15:00:29 +0100

    Update project with own firebase api account connection

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+      const response = await fetch('https://react-http-8f15e-default-rtdb.europe-west1.firebasedatabase.app/tasks.json');
 
       if (!response.ok) {
         throw new Error('Request failed!');
